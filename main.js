0bf4dce01a464ea2d2c20d0f76f2e99cf8008d15
James M <james.mullaney33@gmail.com>
2020-02-18 12:17:38 -0600

    calculations

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-btnElement.onclick = async () -> {
-  const totalAmt = parseInt(document.querySelector('#total').value)
+function calc(){
+  const mealTot = parseInt(document.getElementById("total").value)
+  const taxRate = parseInt(document.getElementById("rate").value)
+  document.getElementById('tip').innerHTML = mealTot * (taxRate/100)
 }
