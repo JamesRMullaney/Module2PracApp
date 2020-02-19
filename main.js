66c4a4e04c7a891ec1eee550253be5c9219c90f0
James M <james.mullaney33@gmail.com>
2020-02-18 23:04:38 -0600

    spacing

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ function calc(){
   const mealTot = parseInt(document.getElementById("total").value)
   const taxRate = parseInt(document.getElementById("rate").value)
   document.getElementById('tip').innerHTML = mealTot * (taxRate/100)
-  document.getElementById('tip').innerHTML = mealTot * (5.5/100)
-  document.getElementById('final').innerHTML = mealTot + (mealTot * (taxRate/100) + (mealTot * (5.5/100)))
+  document.getElementById('tax').innerHTML = mealTot * (.055)
+  document.getElementById('final').innerHTML = mealTot + (mealTot * (taxRate/100)) + (mealTot * (.055))
 }
