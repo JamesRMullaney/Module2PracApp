74263ecc5f08849783034826935ac6ab20b6ca80
James M <james.mullaney33@gmail.com>
2020-02-18 23:23:46 -0600

    this is it

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ function calc(){
   const mealTot = parseInt(document.getElementById("total").value)
   const taxRate = parseInt(document.getElementById("rate").value)
   const tipNum = mealTot * (taxRate/100)
+  const taxNum = mealTot * (.055)
+  const finalNum = mealTot + (mealTot * (taxRate/100)) + (mealTot * (.055))
   document.getElementById('tip').innerHTML = tipNum.toFixed(2)
-  document.getElementById('tax').innerHTML = mealTot * (.055)
-  document.getElementById('final').innerHTML = mealTot + (mealTot * (taxRate/100)) + (mealTot * (.055))
+  document.getElementById('tax').innerHTML = taxNum.toFixed(2)
+  document.getElementById('final').innerHTML = finalNum.toFixed(2)
 }
