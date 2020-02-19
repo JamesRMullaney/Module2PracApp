63483f83d06c7204852fee2bcfbb64e38d72775f
James M <james.mullaney33@gmail.com>
2020-02-18 22:52:30 -0600

    js stuff

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ function calc(){
   const mealTot = parseInt(document.getElementById("total").value)
   const taxRate = parseInt(document.getElementById("rate").value)
   document.getElementById('tip').innerHTML = mealTot * (taxRate/100)
+  document.getElementById('tax').innerHTML = mealTot + (mealTot * (taxRate/100))
 }
